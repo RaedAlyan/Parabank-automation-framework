f539cb10680b229af6f4121fd45acd7ff3973ebc
Raed Eleyan <raedeleyan1@gmail.com>
2025-04-18 22:20:13 +0300

    Updated the test_register_page test file

--- tests/test_register_page.py
+++ tests/test_register_page.py
@@ -6,17 +6,18 @@ Test module for the Register Page.
 @contact: raedeleyan1@gmail.com
 """
 import pytest
+from selenium.webdriver.remote.webdriver import WebDriver
 from pages.register_page import RegisterPage
 
 
-@pytest.mark.usefixtures('browser')
+@pytest.mark.order(1)
 class TestRegisterPage:
     """Test suite for user registration functionality."""
 
-    def test_register_new_user(self, register_data: dict):
+    def test_register_new_user(self, browser: WebDriver, register_data: dict):
         """Test case to verify that a new user can register successfully."""
-        self.driver.get('https://parabank.parasoft.com/parabank/register.htm')
-        register_page: RegisterPage = RegisterPage(self.driver)
+        browser.get('https://parabank.parasoft.com/parabank/register.htm')
+        register_page = RegisterPage(browser)
         register_page.register_user(user_data=register_data)
         expected_welcome_message: str = f'Welcome {register_data["username"]}'
         actual_welcome_message: str = register_page.get_welcome_message()
@@ -26,12 +27,14 @@ class TestRegisterPage:
 
     @pytest.mark.parametrize("missing_field", ["first_name", "last_name", "address", "city", "state", "zip_code", "ssn",
                                                "username", "password", "confirm_password"])
-    def test_register_with_missing_required_field_input(self, register_data: dict, missing_field: str):
+    def test_register_with_missing_required_field_input(self, browser: WebDriver, register_data: dict,
+                                                        missing_field: str):
         """Test case to verify registration fails when a required field is missing."""
-        self.driver.get('https://parabank.parasoft.com/parabank/register.htm')
-        register_data.pop(missing_field)
-        register_page: RegisterPage = RegisterPage(self.driver)
-        register_page.register_user(user_data=register_data)
+        browser.get('https://parabank.parasoft.com/parabank/register.htm')
+        user_data = register_data.copy()
+        user_data.pop(missing_field)
+        register_page = RegisterPage(browser)
+        register_page.register_user(user_data=user_data)
         popup_error_message: str = register_page.get_popup_error_message(missing_field)
         assert popup_error_message is not None, (
             f'Expected an error message for missing field "{missing_field}", but none was found.'
